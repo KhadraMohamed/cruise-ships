13f7ec10a2719e062d45f5877743a840b91ca440
khadra mohamed <khadramohamed@live.co.uk>
2022-04-15 18:28:06 +0100

    Added Set Sail Prototype

--- src/Ship.js
+++ src/Ship.js
@@ -2,5 +2,13 @@ function Ship (name) {
     this.ship = Ship;
     this.startingPort = 'Dover';
 }
+Ship.prototype.setSail = function () {
+    this.setSail = false
+}
+
+
+
+
+
 
 module.exports = Ship;
